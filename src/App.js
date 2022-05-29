a8ab0d24bddd87deeab5fae279bf40b9bac1433b
Avinash Kumar <avi8654340@gmail.com>
2022-05-29 21:04:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{ useState } from 'react';
+import './App.css';
+import FeaturedProducts from './FeaturedProducts';
+
+
+function App() {
+return (
+	<div className='App'>
+      <FeaturedProducts />
+    </div>
+);
+}
+
+export default App;
